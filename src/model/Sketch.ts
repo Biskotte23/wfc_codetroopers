c87f402166cb38e043ac12981482ee9878d6a527
Jules Pascot <jules.pascot@gmail.com>
2023-06-21 22:55:44 +0200

    refactor(draw): refactoring of draw function

--- src/model/Sketch.ts
+++ src/model/Sketch.ts
@@ -1,4 +1,6 @@
 import Cell from "./Cell";
+import * as p5 from "p5";
+import Tile from "./Tile";
 
 class Sketch {
     width: number;
@@ -6,7 +8,6 @@ class Sketch {
     grid: Cell[];
 
     constructor(width: number, height: number) {
-        console.log("Test");
         this.width = width;
         this.height = height;
         this.grid = [];
@@ -25,6 +26,16 @@ class Sketch {
         }
     }
 
+    public isGridFilled(): boolean {
+        return this.getEmptyCells().length === 0;
+    }
+
+    public getEmptyCells(): Cell[] {
+        let gridCopy: Cell[] = this.grid.slice();
+        let emptyCells: Cell[] = gridCopy.filter((a) => !a.collapsed);
+        return emptyCells;
+    }
+
     public checkValid(arr: number[], valid: number[]): void {
         for (let i = arr.length - 1; i >= 0; i--) {
             let element = arr[i];
@@ -32,6 +43,128 @@ class Sketch {
                 arr.splice(i, 1);
             }
         }
+    }
+
+    public defineNextTileToDraw(p: p5): boolean {
+        let isNextTileDefined = true;
+        let emptyCells: Cell[] = this.getEmptyCells();
+
+        // Sort empty cells in ascending order of number of tile options.
+        emptyCells.sort((a, b) => {
+            return a.options.length - b.options.length;
+        });
+
+        // Number of options in the empty cell with the fewest options.
+        let len = emptyCells[0].options.length;
+
+        // Number of cells with the same minimum number of options.
+        let stopIndex = 0;
+
+        for (let i = 1; i < emptyCells.length; i++) {
+            if (emptyCells[i].options.length > len) {
+                stopIndex = i;
+                break;
+            }
+        }
+
+        // Deletes all cells with a non-minimal number of options.
+        if (stopIndex > 0) emptyCells.splice(stopIndex);
+
+        // Random selection of the next cell from those with a minimum number of options.
+        const cell = p.random(emptyCells);
+        cell.collapsed = true;
+
+        // Random selection of the cell tile.
+        const pick = p.random(cell.options);
+
+        if (pick === undefined) {
+            // If no tile is available, we rebuild the grid.
+            this.createGrid(emptyCells.length);
+            isNextTileDefined = false;
+        } else {
+            // The cell now has only one tile, the selected one.
+            cell.options = [pick];
+        }
+
+        return isNextTileDefined;
+    }
+
+    public createNextGrid(tiles: Tile[]) {
+        // Construction de la prochaine grille.
+        const nextGrid = [];
+        for (let line = 0; line < this.height; line++) {
+            for (let column = 0; column < this.width; column++) {
+                let index = column + line * this.height;
+
+                if (this.grid[index].collapsed) {
+                    // Si la cell n'est pas vide, alors on la laisse telle quelle.
+                    nextGrid[index] = this.grid[index];
+                } else {
+                    // Si la cell est vide, alors on lui attribue.
+
+                    // Création d'un tableau avec les index de toutes les tiles.
+                    let options = new Array(tiles.length)
+                        .fill(0)
+                        .map((x, i) => i);
+
+                    // UP.
+                    if (line > 0) {
+                        let up = this.grid[column + (line - 1) * this.height];
+                        let validOptions: any[] = [];
+
+                        for (let option of up.options) {
+                            let valid = tiles[option].down;
+                            validOptions = validOptions.concat(valid);
+                        }
+
+                        this.checkValid(options, validOptions);
+                    }
+
+                    // RIGHT.
+                    if (column < this.width - 1) {
+                        let right = this.grid[column + 1 + line * this.height];
+                        let validOptions: any[] = [];
+
+                        for (let option of right.options) {
+                            let valid = tiles[option].left;
+                            validOptions = validOptions.concat(valid);
+                        }
+
+                        this.checkValid(options, validOptions);
+                    }
+
+                    // DOWN.
+                    if (line < this.height - 1) {
+                        let down = this.grid[column + (line + 1) * this.height];
+                        let validOptions: any[] = [];
+
+                        for (let option of down.options) {
+                            let valid = tiles[option].up;
+                            validOptions = validOptions.concat(valid);
+                        }
+
+                        this.checkValid(options, validOptions);
+                    }
+
+                    // LEFT.
+                    if (column > 0) {
+                        let left = this.grid[column - 1 + line * this.height];
+                        let validOptions: any[] = [];
+
+                        for (let option of left.options) {
+                            let valid = tiles[option].right;
+                            validOptions = validOptions.concat(valid);
+                        }
+
+                        this.checkValid(options, validOptions);
+                    }
+
+                    nextGrid[index] = new Cell(options);
+                }
+            }
+        }
+
+        this.grid = nextGrid;
     }
 
     public getDimension(): number {
--- src/sketch/Sketch.tsx
+++ src/sketch/Sketch.tsx
@@ -1,23 +1,13 @@
 import React, { useEffect, useRef } from "react";
 import p5 from "p5";
 import Tile, { TileSetJSON } from "../model/Tile";
-import Cell from "../model/Cell";
 import tileSetJSON from "../assets/json/tiles/platformer-industrial-expansion.json";
 import Sketch from "model/Sketch";
+import "./sketch.css";
 
-const SKETCH_WIDTH = 20;
+const SKETCH_WIDTH = 10;
 const SKETCH_HEIGHT = 10;
-const TILE_SIZE = 30;
-
-// Retire de arr les options non présentes dans valid.
-function checkValid(arr: number[], valid: number[]): void {
-    for (let i = arr.length - 1; i >= 0; i--) {
-        let element = arr[i];
-        if (!valid.includes(element)) {
-            arr.splice(i, 1);
-        }
-    }
-}
+const CELL_SIZE = 30;
 
 interface SketchProps {}
 
@@ -43,8 +33,8 @@ const SketchElement: React.FC<SketchProps> = () => {
             p.setup = () => {
                 // Creates sketch canvas.
                 const canvas = p.createCanvas(
-                    sketch.width * TILE_SIZE,
-                    sketch.height * TILE_SIZE
+                    sketch.width * CELL_SIZE,
+                    sketch.height * CELL_SIZE
                 );
                 canvas.parent(
                     canvasRef.current?.parentNode as HTMLCanvasElement
@@ -57,166 +47,47 @@ const SketchElement: React.FC<SketchProps> = () => {
             p.draw = () => {
                 p.background(0);
 
-                // Dimensions de la cell.
+                // Cell dimension.
                 const cellWidth = p.width / sketch.width;
-                const h = p.height / sketch.height;
+                const cellHeight = p.height / sketch.height;
 
-                // Affichage de la grille et des tiles déjà dessinées.
+                // Draws cell grid.
                 for (let line = 0; line < sketch.height; line++) {
                     for (let column = 0; column < sketch.width; column++) {
                         let cell = sketch.grid[line * sketch.height + column];
 
                         if (cell.collapsed) {
-                            // Tile affichée.
-
-                            // Sélection de la tile parmi les options.
-                            // Le passer en random ?
+                            // Drawn cell.
                             let index = cell.options[0];
-
                             p.image(
-                                // tiles[index].imagePath,
                                 tileImages[index],
                                 column * cellWidth,
-                                line * h,
+                                line * cellHeight,
                                 cellWidth,
-                                h
+                                cellHeight
                             );
                         } else {
-                            // Cell vide.
+                            // Empty cell.
                             p.noFill();
                             p.stroke(51);
-                            p.rect(column * cellWidth, line * h, cellWidth, h);
+                            p.rect(
+                                column * cellWidth,
+                                line * cellHeight,
+                                cellWidth,
+                                cellHeight
+                            );
                         }
                     }
                 }
 
-                let gridCopy = sketch.grid.slice();
-                // Récupération des cells vides.
-                gridCopy = gridCopy.filter((a) => !a.collapsed);
-
-                // Si grille remplie.
-                if (gridCopy.length === 0) {
+                // If the grid is filled, generation stops.
+                if (sketch.isGridFilled()) {
                     return;
                 }
 
-                // Tri des cells vides par ordre d'options de tile croissant.
-                gridCopy.sort((a, b) => {
-                    return a.options.length - b.options.length;
-                });
-
-                // Nombre d'options de la cell vide avec le moins d'options.
-                let len = gridCopy[0].options.length;
-
-                // Nombre de cells avec le même nombre minimal d'options.
-                let stopIndex = 0;
-
-                for (let i = 1; i < gridCopy.length; i++) {
-                    if (gridCopy[i].options.length > len) {
-                        stopIndex = i;
-                        break;
-                    }
+                if (sketch.defineNextTileToDraw(p)) {
+                    sketch.createNextGrid(tiles);
                 }
-
-                // Supprime toutes les celles avec un nombre d'options non minimal.
-                if (stopIndex > 0) gridCopy.splice(stopIndex);
-
-                // Sélection au hasard de la prochaine cellule à partir de celles ayant un nombre minimal d'options.
-                const cell = p.random(gridCopy);
-                cell.collapsed = true;
-
-                // Sélection au hasard de la tile de la cell.
-                const pick = p.random(cell.options);
-
-                // Si aucune tile n'est disponible, alors on rebuild la grille.
-                if (pick === undefined) {
-                    sketch.createGrid(tiles.length);
-                    return;
-                }
-
-                // La cell n'a plus qu'une seule tile, celle sélectionnée.
-                cell.options = [pick];
-
-                // Construction de la prochaine grille.
-                const nextGrid = [];
-                for (let line = 0; line < sketch.height; line++) {
-                    for (let column = 0; column < sketch.width; column++) {
-                        let index = column + line * sketch.height;
-
-                        if (sketch.grid[index].collapsed) {
-                            // Si la cell n'est pas vide, alors on la laisse telle quelle.
-                            nextGrid[index] = sketch.grid[index];
-                        } else {
-                            // Si la cell est vide, alors on lui attribue.
-
-                            // Création d'un tableau avec les index de toutes les tiles.
-                            let options = new Array(tiles.length)
-                                .fill(0)
-                                .map((x, i) => i);
-
-                            // UP.
-                            if (line > 0) {
-                                let up =
-                                    sketch.grid[
-                                        column + (line - 1) * sketch.height
-                                    ];
-                                let validOptions: any[] = [];
-
-                                for (let option of up.options) {
-                                    let valid = tiles[option].down;
-                                    validOptions = validOptions.concat(valid);
-                                }
-
-                                checkValid(options, validOptions);
-                            }
-
-                            // RIGHT.
-                            if (column < sketch.width - 1) {
-                                let right =
-                                    sketch.grid[
-                                        column + 1 + line * sketch.height
-                                    ];
-                                let validOptions: any[] = [];
-                                for (let option of right.options) {
-                                    let valid = tiles[option].left;
-                                    validOptions = validOptions.concat(valid);
-                                }
-                                checkValid(options, validOptions);
-                            }
-
-                            // DOWN.
-                            if (line < sketch.height - 1) {
-                                let down =
-                                    sketch.grid[
-                                        column + (line + 1) * sketch.height
-                                    ];
-                                let validOptions: any[] = [];
-                                for (let option of down.options) {
-                                    let valid = tiles[option].up;
-                                    validOptions = validOptions.concat(valid);
-                                }
-                                checkValid(options, validOptions);
-                            }
-
-                            // LEFT.
-                            if (column > 0) {
-                                let left =
-                                    sketch.grid[
-                                        column - 1 + line * sketch.height
-                                    ];
-                                let validOptions: any[] = [];
-                                for (let option of left.options) {
-                                    let valid = tiles[option].right;
-                                    validOptions = validOptions.concat(valid);
-                                }
-                                checkValid(options, validOptions);
-                            }
-
-                            nextGrid[index] = new Cell(options);
-                        }
-                    }
-                }
-
-                sketch.grid = nextGrid;
             };
         };
 
